66423689c4a1bbe812ed5af44b0eb140ca66a96d
Anton <antondev.test1@gmail.com>
2018-08-17 13:42:32 +0300

    Init project

---
+++ app/src/main/java/com/panda/sleepy/demoforex/MainActivity.java
@@ -0,0 +1,13 @@
+package com.panda.sleepy.demoforex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
